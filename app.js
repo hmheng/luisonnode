7ce28762f5cda18d5b78e57a3f5f712c58f3760e
hmheng <hmheng@pinsland.com>
2018-07-12 23:03:29 +0800

    Project started

---
+++ app.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+var request = require('request');
+var querystring = require('querystring');
+
+function getLuisIntent(utterance) {
+    var endpoint =
+        "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/";
+
+    // Set the LUIS_APP_ID environment variable 
+    // to df67dcdb-c37d-46af-88e1-8b97951ca1c2, which is the ID
+    // of a public sample application.    
+    var luisAppId = '4bf884ed-c9a6-4c50-bf27-3521ac3290b3';
+
+    // Set the LUIS_SUBSCRIPTION_KEY environment variable
+    // to the value of your Cognitive Services subscription key
+    var queryParams = {
+        "subscription-key": '39001e52b8ab4854ab752211b97ec933',
+        "timezoneOffset": "0",
+        "verbose":  true,
+        "q": utterance
+    }
+
+    var luisRequest =
+        endpoint + luisAppId +
+        '?' + querystring.stringify(queryParams);
+
+    request(luisRequest,
+        function (err,
+            response, body) {
+            if (err)
+                console.log(err);
+            else {
+                var data = JSON.parse(body);
+
+                console.log(`Query: ${data.query}`);
+                console.log(`Top Intent: ${data.topScoringIntent.intent}`);
+                console.log('Intents:');
+                console.log(data.intents);
+            }
+        });
+}
+
+// Pass an utterance to the sample LUIS app
+getLuisIntent('what\'s the hawaiian pizza made of');
